0437d9d2dbdda263477a93a087b25200362195d4
sid <s.sidd707@gmail.com>
2022-10-13 21:17:48 +0545

    minor changes

--- demo_app/lib/logged_in.dart
+++ demo_app/lib/logged_in.dart
@@ -15,9 +15,9 @@ class _LoggedinState extends State<Loggedin> {
       appBar: AppBar(
         title: Text('Your Profile'),
       ),
-      body: Stack(children: [
-        Column(
-          children: [
+      body: Column(
+        children: [
+          Stack(children: [
             Container(
               height: size.height * 0.4,
               width: size.width,
@@ -31,31 +31,40 @@ class _LoggedinState extends State<Loggedin> {
                 ),
               ),
             ),
-          ],
-        ),
-        Positioned(
-            top: size.height * 0.15,
-            left: size.width * 0.15,
-            child: Container(
-              height: 25,
-              width: 25,
-              color: Colors.tealAccent,
-            )),
-        Center(
-            child: GestureDetector(
-          onTap: () {
-            setState(() {
-              click = !click;
-            });
-          },
-          child: AnimatedContainer(
-            duration: Duration(seconds: 2),
-            color: click ? Colors.pinkAccent : Colors.lightBlue,
-            height: click ? 150 : 75,
-            width: click ? 75 : 150,
-          ),
-        ))
-      ]),
+            Positioned(
+                top: size.height * 0.15,
+                left: size.width * 0.15,
+                child: Container(
+                  height: 25,
+                  width: 25,
+                  color: Colors.tealAccent,
+                )),
+            Center(
+                child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  click = !click;
+                });
+              },
+              child: AnimatedContainer(
+                duration: Duration(seconds: 2),
+                color: click ? Colors.pinkAccent : Colors.lightBlue,
+                height: click ? 150 : 75,
+                width: click ? 75 : 150,
+              ),
+            ))
+          ]),
+          ListView.builder(
+            itemBuilder: (context, index) {
+              return Container(
+                height: size.height * 0.2,
+                color: Colors.redAccent,
+                padding: EdgeInsets.all(5),
+              );
+            },
+          )
+        ],
+      ),
     );
   }
 }
